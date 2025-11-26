0a60e2f79d3c70612b48e5e1f4bb1ac67b328284
Patil-Gitanjali <Gitanjalipatil2204@gmail.com>
2025-11-26 11:49:20 +0530

    Add files via upload

---
+++ Program166.java
@@ -0,0 +1,50 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+//  Function Name :  Display
+//  Description :    Pattern  Printing
+//  Input :          irow:4
+//                   iColumn:4
+//  Output :         1  2  3  4
+//                   1  2  3  4
+//                   1  2  3  4 
+//                   1  2  3  4
+//  Author :        Gitanjali Meghanath Patil
+//  Date :          21/11/2025
+// 
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print(j+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Program166
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter the number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+    }
+}
---
+++ Program168.java
@@ -0,0 +1,57 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+//  Function Name :  Display
+//  Description :    Pattern  Printing
+//  Input :          irow:4
+//                   iColumn:4
+//  Output :         *  *  *  *
+//                   $  $  $  $
+//                   *  *  *  *
+//                   $  $  $  $
+//  Author :        Gitanjali Meghanath Patil
+//  Date :          21/11/2025
+// 
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if((i % 2) == 0)
+                {
+                    System.out.print("$\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Program168
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+    }
+}
